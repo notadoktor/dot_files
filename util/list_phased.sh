50585c7a31e7f9ac9f0f37ccaf277a3b44bc1934
notadoktor <notadoktor@gmail.com>
2023-03-08 17:41:02 +0100

    list_phased

---
+++ util/list_phased.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+mapfile -t avail_pkgs < <(apt list --upgradable 2>/dev/null | grep / | cut -f1 -d/)
+
+if [[ ${#avail_pkgs[@]} -eq 0 ]]; then
+    echo "No packages available for upgrade"
+    exit 0
+fi
+max_len=0
+declare -A phased_pkgs
+for pkg_name in "${avail_pkgs[@]}"; do
+    phased=$(apt-cache policy "$pkg_name" | grep -oP '(phased \d+%)' || true)
+    if [[ -n $phased ]]; then
+        if ((${#pkg_name} > max_len)); then
+            max_len=${#pkg_name}
+        fi
+        phased_pkgs["$pkg_name"]="$phased"
+    fi
+done
+
+if ((${#phased_pkgs[@]} == 0)); then
+    echo "No packages are phased"
+    exit 0
+elif ((${#phased_pkgs[@]} == ${#avail_pkgs[@]})); then
+    echo "All available packages are phased"
+else
+    echo "${#phased_pkgs[@]} of ${#avail_pkgs[@]} packages are phased"
+fi
+for pkg in "${!phased_pkgs[@]}"; do
+    printf "  %-${max_len}s  %s\n" "$pkg" "${phased_pkgs[$pkg]}"
+done | sort
